4d5cd68ffb9e35804527f2b1640e095768bbbe2a
Yanic Nef <e282264@edu.sbl.ch>
2023-11-24 09:09:58 +0100

    Aufgabe 4 gelöst

--- script.js
+++ script.js
@@ -36,3 +36,13 @@ export function aufgabe03(args) {
   }
   return count
 }
+export function aufgabe04(args) {
+  const input = args
+  const result = []
+  let count = 0
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    count++
+  }
+  return count
+}
