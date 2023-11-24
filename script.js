63c453bc68a3f543fd3a4bd7cb6f825ca757342f
Yanic Nef <e282264@edu.sbl.ch>
2023-11-24 08:52:25 +0100

    Aufgabe 3 gelöst

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ export function aufgabe01(args) {
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
     if(currentElement === "e" || currentElement === "E") {
-      
+
     }
     else
     {result.push(currentElement)}
@@ -26,9 +26,13 @@ export function aufgabe02(args) {
 export function aufgabe03(args) {
   const input = args
   const result = []
+  let count = 0
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    
+     if(currentElement === "e" || currentElement === "E") {
+      count++
+    }
+
   }
-  return result.join("")
+  return count
 }
